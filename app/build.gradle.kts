a8e586333acb3b107ef2501eb8cb5e978b49df65
Ashkan Ansarifard <ansarifardashkan@gmail.com>
2024-06-06 22:23:12 +0200

    Implements google sign in

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,6 +71,7 @@ dependencies {
     implementation(libs.androidx.annotation)
     implementation(libs.androidx.lifecycle.livedata.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.google.googleid)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -82,4 +83,8 @@ dependencies {
     implementation(libs.retrofit2)
     implementation(libs.retrofit2.converter.gson)
     implementation(libs.logging.interceptor)
+    implementation(libs.opencv)
+    implementation(libs.androidx.credentials)
+    implementation(libs.androidx.credentials.play.services.auth)
+    implementation(libs.googleid)
 }
--- app/src/main/java/io/ashkanans/artwalk/LoginActivity.kt
+++ app/src/main/java/io/ashkanans/artwalk/LoginActivity.kt
@@ -1,12 +1,28 @@
 package io.ashkanans.artwalk
 
 import LoginAdapter
+import android.content.ContentValues.TAG
+import android.content.Intent
+import android.credentials.GetCredentialException
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.credentials.CredentialManager
+import androidx.credentials.CustomCredential
+import androidx.credentials.GetCredentialRequest
+import androidx.credentials.GetCredentialResponse
+import androidx.lifecycle.lifecycleScope
 import androidx.viewpager.widget.ViewPager
+import com.google.android.libraries.identity.googleid.GetGoogleIdOption
+import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
+import com.google.android.libraries.identity.googleid.GoogleIdTokenParsingException
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.tabs.TabLayout
+import kotlinx.coroutines.launch
+import java.util.UUID
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var tabLayout: TabLayout
@@ -14,6 +30,7 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var google: FloatingActionButton
     private lateinit var loginButton: Button
 
+    @RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -37,6 +54,75 @@ class LoginActivity : AppCompatActivity() {
         google.animate().translationY(0f).alpha(1f).setDuration(1000).setStartDelay(400).start()
         tabLayout.animate().translationY(0f).alpha(1f).setDuration(1000).setStartDelay(400).start()
 
+        // Set up Google Sign-In
+        google.setOnClickListener {
+            val WEB_CLIENT_ID =
+                "904815380413-g5jfhmjbflmv4d24ejnbdo0qqtmrnjbp.apps.googleusercontent.com"
+            val googleIdOption: GetGoogleIdOption = GetGoogleIdOption.Builder()
+                .setFilterByAuthorizedAccounts(false)
+                .setServerClientId(WEB_CLIENT_ID)
+                .setAutoSelectEnabled(true)
+                .setNonce(generateNonce())
+                .build()
+
+            val request: GetCredentialRequest = GetCredentialRequest.Builder()
+                .addCredentialOption(googleIdOption)
+                .build()
+
+            val credentialManager = CredentialManager.create(this@LoginActivity)
+            lifecycleScope.launch {
+                try {
+                    Log.e("request.. ===", request.toString())
+                    val result = credentialManager.getCredential(
+                        request = request,
+                        context = this@LoginActivity,
+                    )
+                    handleSignIn(result)
+                } catch (e: GetCredentialException) {
+                    handleFailure(e)
+                }
+            }
+
+        }
     }
 
+    private fun handleSignIn(result: GetCredentialResponse) {
+        val credential = result.credential
+
+        when (credential) {
+            is CustomCredential -> {
+                if (credential.type == GoogleIdTokenCredential.TYPE_GOOGLE_ID_TOKEN_CREDENTIAL) {
+                    try {
+                        val googleIdTokenCredential =
+                            GoogleIdTokenCredential.createFrom(credential.data)
+                        val idToken = googleIdTokenCredential.idToken
+
+                        startActivity(Intent(this, MapsActivity::class.java))
+                        finish()
+                    } catch (e: GoogleIdTokenParsingException) {
+                        Log.e(TAG, "Received an invalid google id token response", e)
+                    }
+                } else {
+                    Log.e(TAG, "Unexpected type of credential")
+                }
+            }
+
+            else -> {
+                Log.e(TAG, "Unexpected type of credential")
+            }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
+    private fun handleFailure(e: GetCredentialException) {
+        Log.e(TAG, "Failed to get credentials", e)
+    }
+
+    private fun generateNonce(): String {
+        return UUID.randomUUID().toString()
+    }
+
+//    fun signOut() {
+//        CredentialManager.clearCredentialState(this@LoginActivity, WEB_CLIENT_ID)
+//    }
 }
