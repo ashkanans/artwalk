4090f911737d5f082a92c9842efca6a1b0171a53
Ashkan Ansarifard <ansarifardashkan@gmail.com>
2024-06-14 00:18:37 +0200

    Improves LibraryDashboardFragment cards UI

--- app/src/main/java/io/ashkanans/artwalk/LibraryDashboardFragment.kt
+++ app/src/main/java/io/ashkanans/artwalk/LibraryDashboardFragment.kt
@@ -1,9 +1,11 @@
 package io.ashkanans.artwalk
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AccelerateDecelerateInterpolator
 import androidx.fragment.app.Fragment
 import io.ashkanans.artwalk.databinding.FragmentLibraryDashboardBinding
 
@@ -16,6 +18,10 @@ class LibraryDashboardFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentLibraryDashboardBinding.inflate(inflater, container, false)
+        binding.googlePlacesCard.background = null
+        binding.youtubeCard.background = null
+        binding.spotifyCard.background = null
+        binding.wikiCard.background = null
         return binding.root
     }
 
@@ -23,7 +29,23 @@ class LibraryDashboardFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val caption = arguments?.getString("CAPTION")
-        binding.landmarkName.text = arguments?.getString("CAPTION")
+        binding.landmarkName.text = caption
+
+        // Animate the cards
+        animateCard(binding.googlePlacesCard, -binding.googlePlacesCard.width.toFloat(), 0f)
+        animateCard(binding.spotifyCard, -binding.spotifyCard.width.toFloat(), 0f)
+        animateCard(binding.youtubeCard, binding.youtubeCard.width.toFloat(), 0f)
+        animateCard(binding.wikiCard, binding.wikiCard.width.toFloat(), 0f)
+    }
+
+    private fun animateCard(view: View, fromX: Float, toX: Float) {
+        view.translationX = fromX
+        view.visibility = View.VISIBLE
+        ObjectAnimator.ofFloat(view, "translationX", fromX, toX).apply {
+            duration = 1000
+            interpolator = AccelerateDecelerateInterpolator()
+            start()
+        }
     }
 
     override fun onDestroyView() {
